16194936c387b51d199b40776082fd7284f016f6
ngmtine <ngmfries@gmail.com>
2021-04-12 15:54:28 +0900

    fish: ペイン内にフォアグラウンドのnvimある場合のアタッチ（？）を追加、（バックグラウンド時の挙動は未実装）

--- userhome/.config/fish/config.fish
+++ userhome/.config/fish/config.fish
@@ -7,11 +7,25 @@ abbr -a vi nvim
 
 function nvim -d "tmuxの同一ウインドウ内にnvimが起動済みならばそちらで開きます"
 	if test -z $TMUX
+		# tmuxの外ならば、そのままnvimを起動する
 		command nvim $argv
 	else
 		set socket_path /tmp/(echo (tmux display-message -p -F "#{session_id}#{window_index}nvim"))
 		if test -S $socket_path
-			nvr --remote-tab --servername $socket_path $argv
+			# 既にnvim起動中ならば
+			set IFS
+			set all_nvim_proc (ps -efl | grep [n]vim) # セッション問わず起動中のnvimプロセス全て
+			set IFS \n" "\t
+			for pane_start_pid in (string split ' ' (echo (tmux list-panes -F "#{pane_pid}")))
+				# このウィンドウ内に存在する、全てのペインの起動時pidを列挙
+				if test (echo $all_nvim_proc | awk -v psp=$pane_start_pid '{if ($5==psp && $2=="S") print 0}')
+					# このウインドウ内にフォアグラウンドのnvimが存在する
+					nvr --remote-tab --servername $socket_path $argv
+				else if test (echo $all_nvim_proc | awk -v psp=$pane_start_pid '{if ($5==psp && $2=="T") print 0}')
+					# このウインドウ内にバックグラウンドのnvimが存在する
+					echo フォアグラウンドにもってきてnvrする
+				end
+			end
 		else
 			command env NVIM_LISTEN_ADDRESS=$socket_path nvim $argv
 		end
@@ -20,20 +34,4 @@ end
 
 abbr -a cdd cd /mnt/d/win/denon
 # abbr -a yp youtube-dl --download-archive ./downloaded.txt
-
-
-function nvim_test
-	set IFS
-	set all_nvim_proc (ps -efl | grep [n]vim) # セッション問わず起動中のnvimプロセス全て
-	set IFS \n" "\t
-
-	for pane_start_pid in (string split ' ' (echo (tmux list-panes -F "#{pane_pid}")))
-		# このウィンドウ内に存在する、全てのペインの起動時pidを列挙
-		if test (echo $all_nvim_proc | awk -v psp=$pane_start_pid '{if ($5==psp && $2=="S") print 0}')
-			# このウインドウ内にフォアグラウンドのnvimが存在する
-		else if test (echo $all_nvim_proc | awk -v psp=$pane_start_pid '{if ($5==psp && $2=="T") print 0}')
-			# このウインドウ内にバックグラウンドのnvimが存在する
-		end
-	end
-end
 
