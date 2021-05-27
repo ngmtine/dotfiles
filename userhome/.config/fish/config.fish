9bb574544b498f7c954ac4e00385c9a109af9b6b
ngmtine <ngmfries@gmail.com>
2021-05-27 15:25:39 +0900

    fish: mkmainpyをつくった（acc newのあとに実行すると吉

--- userhome/.config/fish/config.fish
+++ userhome/.config/fish/config.fish
@@ -8,3 +8,18 @@ abbr -a vi nvim
 abbr -a cdd cd /mnt/d/win/denon
 # abbr -a yp youtube-dl --download-archive ./downloaded.txt
 
+function mkmainpy
+	# 直下のディレクトリにmain.pyとinput.txtを作る
+	for dir in (find ./ -maxdepth 1 -mindepth 1 -type d)
+		touch $dir/input.txt
+		if not test -e $dir/main.py
+			touch $dir/main.py
+		end
+	end
+	# vscodeの統合ターミナルから実行する場合はvscodeで開く
+	if test "$TERM_PROGRAM" = "vscode"
+		for mainpy in (find ./ -name main.py | sort)
+			code $mainpy
+		end
+	end
+end
