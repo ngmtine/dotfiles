ea26d25f56fc64b67cf18dadaf7049e4040f4215
ngmtine <ngmfries@gmail.com>
2021-06-08 17:20:58 +0900

    fish: mkmainpyを追加

--- userhome/.config/fish/config.fish
+++ userhome/.config/fish/config.fish
@@ -20,3 +20,21 @@ end
 
 abbr -a cdd cd /mnt/d/win/denon
 # abbr -a yp youtube-dl --download-archive ./downloaded.txt
+
+function mkmainpy
+	if test "$TERM_PROGRAM" = "vscode"
+		for dir in (find ./ -maxdepth 1 -mindepth 1 -type d)
+			if not test -e $dir/main.py
+				touch $dir/main.py
+			end
+			if not test -e $dir/input.txt
+				touch $dir/input.txt
+			end
+		end
+	for mainpy in (find ./ -name main.py | sort)
+		code $mainpy
+	end	
+	else
+		echo "vscode上で実行してね～"
+	end
+end
