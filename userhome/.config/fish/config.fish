6e424d9cbc49db1e9c2cf5e8eb05a688dbb63a67
ngmtine <ngmfries@gmail.com>
2021-09-08 23:42:40 +0900

    fish: dockerとgitのエイリアスを設定したけどまだまだ中途半端

--- userhome/.config/fish/config.fish
+++ userhome/.config/fish/config.fish
@@ -5,11 +5,32 @@ export EDITOR="/usr/bin/nvim"
 abbr -a e explorer.exe
 abbr -a vi nvim
 
+# gitのエイリアス
+# ~/.gitconfig を編集する手もあるけど、fishと違い実行時に展開されない（エイリアスなので当たり前だけど）
+abbr -a g git
+abbr -a gad git add
+abbr -a gcm git commit -m \"
+abbr -a gps git push
+abbr -a gcl git clone
+abbr -a gck git checkout
+abbr -a gbr git branch -a
+abbr -a gst git status
+abbr -a gfe git fetch
+
+# dockerのエイリアス
+abbr -a d docker
+abbr -a dps docker container ls
+abbr -a dls docker container ls
+abbr -a dcp docker container cp
+abbr -a dex docker container exec
+# abbr -a dc docker container
+# abbr -a di docker image
+
 abbr -a cdd cd /mnt/d/win/denon
 # abbr -a yp youtube-dl --download-archive ./downloaded.txt
 
 function mkmainpy
-	if test "$TERM_PROGRAM" = "vscode"
+if test "$TERM_PROGRAM" = "vscode"
 		for dir in (find ./ -maxdepth 1 -mindepth 1 -type d)
 			if not test -e $dir/main.py
 				touch $dir/main.py
