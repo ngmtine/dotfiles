ba9dcee22734291d70964432c7affa3d36d99ad1
ngmtine <ngmfries@gmail.com>
2024-12-19 00:31:49 +0900

    wezterm: 外見ちょっとかえた

--- userhome/.wezterm.lua
+++ userhome/.wezterm.lua
@@ -1,7 +1,14 @@
 local wezterm = require 'wezterm';
+local config = wezterm.config_builder()
 
-return {
-  font = wezterm.font("HackGen Console NF", {weight="Regular", stretch="Normal", style="Normal"}), -- /home/nagamon/.local/share/fonts/HackGenConsoleNF-Regular.ttf, FontConfig
-  font_size = 14.0,
-}
+-- font
+config.font = wezterm.font("HackGen Console NF", { weight = "Regular", stretch = "Normal", style = "Normal" })
+config.font_size = 14.0
 
+-- theme
+config.color_scheme = 'iceberg-dark'
+config.window_background_opacity = 0.95
+config.window_decorations = "RESIZE"       -- ウィンドウ最上部のバー非表示
+config.hide_tab_bar_if_only_one_tab = true -- タブ1つのときはタブ非表示
+
+return config
