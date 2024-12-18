0a7e85724f6276e29eef56f2e6b7ab4558da6e1d
ngmtine <ngmfries@gmail.com>
2024-12-18 16:37:51 +0900

    wezterm: add config

---
+++ userhome/.wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm';
+
+return {
+  font = wezterm.font("HackGen Console NF", {weight="Regular", stretch="Normal", style="Normal"}), -- /home/nagamon/.local/share/fonts/HackGenConsoleNF-Regular.ttf, FontConfig
+  font_size = 14.0,
+}
+
