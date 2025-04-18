80750b5b4bb3818bbd8816bc0b9ac144fb042172
ngmtine <ngmfries@gmail.com>
2025-04-18 17:02:41 +0900

    nvim fix: 「vim開く、luaファイル開く、:vnewで新ウィンドウ開く、新ウィンドウでluaファイル開く、元ウィンドウで保存時フォーマットが効かない」のを修正

---
+++ userhome/.config/nvim/lua/plugins_cli/format.lua
@@ -0,0 +1,2 @@
+local format_on_save_augroup = vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
+return format_on_save_augroup
--- userhome/.config/nvim/lua/plugins_cli/lspconfig-lua.lua
+++ userhome/.config/nvim/lua/plugins_cli/lspconfig-lua.lua
@@ -1,19 +1,28 @@
+local format_on_save_augroup = require("plugins_cli/format")
 local keymap_lsp = require("keymap_lsp")
 
 -- luaのlsp設定
 require("lspconfig").lua_ls.setup({
-    on_attach = function(_, bufnr)
+    on_attach = function(client, bufnr)
+        vim.notify(string.format("[lspconfig-lua] LSP client '%s' attached to buffer %d", client.name, bufnr))
+
         -- キーマップ
         keymap_lsp(bufnr)
 
         -- 保存時フォーマット
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("luaFormatting", { clear = true }),
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format({ async = false })
-            end,
-        })
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = format_on_save_augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({
+                        bufnr = bufnr,
+                        async = false,
+                        filter = function(c) return c.name == "lua_ls" end
+                    })
+                end,
+            })
+        end
     end,
 
     -- グローバル変数vimの未定義警告の抑制
